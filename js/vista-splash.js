873b014b744d81f62217c4bb3714bc0dcef50620
“mariaahvgit config --global user.name “mariaahv <mariahv14>
2018-01-11 20:07:24 -0500

    añadiendo animacion al splash

--- js/vista-splash.js
+++ js/vista-splash.js
@@ -4,4 +4,11 @@ function viewSplash() {
   setTimeout(function() {
     window.location.href = 'views/start-page.html';
   }, 5000);
+
+  $(function(){
+    setTimeout(function(){
+      $('.fly-in-text').removeClass('hidden');
+    },500);
+  })();
+
 }
