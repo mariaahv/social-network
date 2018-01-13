c4c170ed296e5b37dbf24d7e6b55da5c08200e94
Danielalab <olabolacrayola@hotmail.com>
2018-01-13 16:11:01 -0500

    registro y login email - firebase

--- js/login.js
+++ js/login.js
@@ -10,16 +10,34 @@ function login() {
   var validInputEmail = false;
   var validInputPasssword = false;   
 
+  var regexEmail = (/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/);
+
   // añade eventos a inputs
   inputEmailLogin.on('input', verifyEmailCredential);   
   inputPasswordLogin.on('input', verifyPasswordCredential);
   btnLogin.on('click', function(event) {
     event.preventDefault();
-    window.location.href = 'profile.html';
+    firebase.auth().signInWithEmailAndPassword(inputEmailLogin.val(), inputPasswordLogin.val())
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+    // observar el cambio de un usuario
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        // User is signed in.        
+        window.location.href = 'profile.html';
+      } else {
+        // User is signed out.
+        console.log('usuario no logueado');
+      }
+    });
   });
 
   function verifyEmailCredential() {
-    if (inputEmailLogin.val() === localStorage.email) {
+    if (regexEmail.test($(inputEmailLogin).val())) {
       console.log('valid email');
       validInputEmail = true;
       allInputsLoginValid();  
@@ -30,7 +48,7 @@ function login() {
   }
 
   function verifyPasswordCredential() {
-    if (inputPasswordLogin.val() === localStorage.password) {
+    if (inputPasswordLogin.val().length >= 6) {
       console.log('valid password');        
       validInputPasssword = true;
       allInputsLoginValid();  
